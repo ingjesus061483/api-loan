3012dc9f70340d66315b228a9e60021aa3a9f966
ingjesus061483 <51796225+ingjesus061483@users.noreply.github.com>
2026-04-19 08:29:28 -0500

    Add edit button column to newness table
    Introduce a leftmost header column and per-row edit button (pencil icon) linking to /Newness/{id}/edit, and remove the duplicate edit column at the end of each row. Keeps existing styling and row data; simplifies the table layout for easier editing.

--- resources/views/Shared/tableNewness.blade.php
+++ resources/views/Shared/tableNewness.blade.php
@@ -1,6 +1,7 @@
      <table  class="table table-hover table-bordered" style="width: 100%" >
             <thead style ="font-size: 14px" >
                 <tr>
+                    <th>&nbsp;&nbsp;&nbsp;&nbsp;</th>
                     <th style="text-align: center">N°</th>
                     <th style="text-align: center">ID</th>
                     <th style="text-align: center">FECHA</th>
@@ -8,13 +9,18 @@
                     <th style="text-align: center">TIPO DE NOVEDAD	</th>
                     <th style="text-align: center">NOVEDAD</th>
                     <th style="text-align: center">STATUS</th>
-                     <th>&nbsp;&nbsp;&nbsp;&nbsp;</th>
+
 
                 </tr>
             </thead>
             <tbody style ="font-size: 12px" >
             @foreach ($newnesses as $item)
             <tr>
+                 <td style="text-align: center;">
+                    <a title="Editar" href="{{url('/Newness/'.$item->id.'/edit')}}" class="btn btn-warning btn-sm">
+                        <i class="fa-solid fa-pencil"></i>
+                    </a>
+                </td>
                 <td style="text-align: center;">{{$item->id}}</td>
                 <td style="text-align: center;"> {{$item->user->name}}</td>
                 <td style="text-align: center;">{{date("d/m/Y", strtotime($item->date))}}</td>
@@ -24,11 +30,7 @@
                 <td style="text-align: center;">
                     <input type="checkbox" name="" id=""{{$item->state_newness->id==2?'checked':''}} onchange="cambiarEstadoNewness({{$item->id}},this)">
                 </td>
-                <td style="text-align: center;">
-                    <a title="Editar" href="{{url('/Newness/'.$item->id.'/edit')}}" class="btn btn-warning btn-sm">
-                        <i class="fa-solid fa-pencil"></i>
-                    </a>
-                </td>
+
             </tr>
             @endforeach
             </tbody>
