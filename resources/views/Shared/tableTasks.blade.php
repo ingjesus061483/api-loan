8aa053b017365b9c719e1dba717c06dc6686357b
ingjesus061483 <51796225+ingjesus061483@users.noreply.github.com>
2026-04-19 08:31:39 -0500

    Move edit button to first column, add STATUS
    Reorder columns in the Shared tasks table view: insert a leading action column that contains the edit button, remove the duplicated edit cell at the end of the row, and replace the empty header with a STATUS header. Adjusts table header/cell order for clearer UX; no backend logic changes.

--- resources/views/Shared/tableTasks.blade.php
+++ resources/views/Shared/tableTasks.blade.php
@@ -1,20 +1,26 @@
       <table  class="table table-hover table-bordered" style="width: 100%" >
             <thead style ="font-size: 14px" >
                 <tr>
+                    <th>&nbsp;&nbsp;&nbsp;&nbsp;</th>
                     <th style="text-align: center">N°</th>
                     <th style="text-align: center">ID</th>
                     <th style="text-align: center">FECHA</th>
                     <th style="text-align: center" >CLIENTE	</th>
                     <th style="text-align: center">TAREA</th>
                     <th style="text-align: center">TIPO DE TAREA</th>
-                    <th style="text-align: center"></th>
-                     <th>&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                    <th style="text-align: center">STATUS</th>
+
 
                 </tr>
             </thead>
             <tbody style ="font-size: 12px" >
             @foreach ($homeworks as $item)
             <tr>
+                <td style="text-align: center;">
+                    <a title="Editar" href="{{url('/homework/'.$item->id.'/edit')}}" class="btn btn-warning btn-sm">
+                        <i class="fa-solid fa-pencil"></i>
+                    </a>
+                </td>
                 <td style="text-align: center;">{{$item->id}}</td>
                 <td style="text-align: center;"> {{$item->user->name}}</td>
                 <td style="text-align: center;">{{date("d/m/Y", strtotime($item->date))}}</td>
@@ -24,11 +30,7 @@
                 <td style="text-align: center;">
                     <input type="checkbox" name="" id=""{{$item->state_homework->id==2?'checked':''}} onchange="cambiarEstadoHomework({{$item->id}},this)">
                 </td>
-                <td style="text-align: center;">
-                    <a title="Editar" href="{{url('/homework/'.$item->id.'/edit')}}" class="btn btn-warning btn-sm">
-                        <i class="fa-solid fa-pencil"></i>
-                    </a>
-                </td>
+
             </tr>
             @endforeach
             </tbody>
